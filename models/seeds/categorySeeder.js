fa5b4faa49e43a94dcdc2b604e9ffbae56bd887c
Jyun-Fu-Chen <than3698741@gmail.com>
2022-03-27 13:36:04 +0800

    feat: add record display feature

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -9,12 +9,21 @@ const categoryList = [
   '餐飲食品',
   '其他'
 ]
+const categoryIconList = [
+  '<i class="fa-solid fa-house fa-xl"></i>',
+  '<i class="fa-solid fa-van-shuttle fa-xl"></i>',
+  '<i class="fa-solid fa-face-grin-beam fa-xl"></i>',
+  '<i class="fa-solid fa-utensils fa-xl"></i>',
+  '<i class="fa-solid fa-pen fa-xl"></i>'
+
+]
 
 db.once('open', () => {
   //建立category資料Ｆ
   for (let i = 0; i < categoryList.length; i++) {
     Category.create({
-      name: categoryList[i]
+      name: categoryList[i],
+      categoryIcon: categoryIconList[i]
     })
   }
   //建立user資料
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
-  res.render('home')
+  const userId = req.user._id
+  Record.find({ userId })
+    .populate('categoryId')
+    .lean()
+    .then(records => {
+      return res.render('home',{records})
+    })
 })
 
 module.exports = router
